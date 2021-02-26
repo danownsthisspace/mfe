cab2c504d172e60c125f48530d449fda8b46d2ae
Daniel Amber <daniel.amber@icloud.com>
2021-02-26 07:19:36 +0200

    init

---
+++ packages/marketing/src/App.js
@@ -0,0 +1,19 @@
+import {BrowserRouter, Route, Switch} from 'react-router-dom'
+
+import Landing from './components/Landing'
+import Pricing from './components/Pricing'
+import React from 'react'
+import { StylesProvider } from '@material-ui/core'
+
+export default () => {
+  return <div>
+    <StylesProvider>
+      <BrowserRouter>
+      <Switch>
+        <Route exact path="/pricing"  component={Pricing}/>
+        <Route exact path="/"  component={Landing}/>
+      </Switch>
+      </BrowserRouter>
+    </StylesProvider>
+  </div>
+}
---
+++ packages/marketing/src/bootstrap.js
@@ -0,0 +1,21 @@
+import App from './App'
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const mount = (el) => {
+  ReactDOM.render(
+    <App />,
+    el
+  )
+}
+
+if (process.env.NODE_ENV === 'development') {
+  const devRoot = document.querySelector('#_marketing-dev-root')
+  if (devRoot) {
+    mount(devRoot)
+  }
+}
+
+export { 
+  mount
+}
