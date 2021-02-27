2248f5155ad9320bab78f708bda92bf094c15ea5
Daniel Amber <daniel.amber@icloud.com>
2021-02-27 17:11:37 +0200

    deploy to firebase

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,12 +1,15 @@
+import { BrowserRouter } from 'react-router-dom'
+import Header from './components/Header'
 import MarketingApp from './components/MarketingApp'
 import React from 'react'
 
 export default () => {
   return (
+  <BrowserRouter>
     <div>
-      <h1>Hi There!</h1>
-      <hr />
+      <Header />
       <MarketingApp />
     </div>
+  </BrowserRouter>
   )
 }
