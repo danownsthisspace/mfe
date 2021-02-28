6929c417298150b65606fa6c93fff66bf2ac568c
Daniel Amber <daniel.amber@icloud.com>
2021-02-28 08:52:20 +0200

    updated styles

--- packages/marketing/src/App.js
+++ packages/marketing/src/App.js
@@ -1,13 +1,17 @@
 import {BrowserRouter, Route, Switch} from 'react-router-dom'
+import { StylesProvider, createGenerateClassName } from '@material-ui/core'
 
 import Landing from './components/Landing'
 import Pricing from './components/Pricing'
 import React from 'react'
-import { StylesProvider } from '@material-ui/core'
+
+const generateClassName = createGenerateClassName({
+  productionPrefix: 'ma'
+})
 
 export default () => {
   return <div>
-    <StylesProvider>
+    <StylesProvider generateClassName={generateClassName}>
       <BrowserRouter>
       <Switch>
         <Route exact path="/pricing"  component={Pricing}/>
